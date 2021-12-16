94ed976d5e2b08787d11a8044af66b07c8379f7a
betolarbac <betocr.dev@gmail.com>
2021-12-15 22:34:52 -0300

    app encapisulado com sessionProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
+import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-       <Header />
-      <Component {...pageProps} />
-    </>
+    
+      <SessionProvider session={pageProps.session} >
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
+    
   )
 }
 
