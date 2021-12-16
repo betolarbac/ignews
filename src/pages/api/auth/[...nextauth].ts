297403f9f024f48c3ab04b8cbfb82022b5782e7e
betolarbac <betocr.dev@gmail.com>
2021-12-15 22:35:49 -0300

    criação e configuração de autenticação e do fauna

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,42 @@
+import {query as q } from 'faunadb';
+
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        authorization: {
+          params: {
+            scope: 'read:user',
+          },
+        },
+      }),
+    // ...add more providers here
+  ],
+ 
+  callbacks: {
+        async signIn({user, account, profile}){
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    q.Create(
+                        q.Collection('users'),
+                        {data: { email } }
+                    )
+                )
+                return true
+            } catch {
+                return false
+            }
+
+        
+      },
+  }
+})
