174b8061967afb1b25b688dadae41f028e35e9ec
betolarbac <betocr.dev@gmail.com>
2021-12-22 00:04:29 -0300

    pagina de posts add e toda função de posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,81 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+
+import Prismic from '@prismicio/client'
+
+import { RichText} from 'prismic-dom'
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+
+//tipagem de posts
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+
+}
+//tipagem de função sendo um array de posts
+interface PostProps {
+    posts: Post[];
+}
+
+//função de posts na tela 
+export default function Posts({posts}: PostProps) {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ig.news </title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    {posts.map(post => (
+                         <a key={post.slug} href="#">
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
+
+                </div>
+            </main>
+        </>
+    );
+}
+
+
+
+
+export const getStaticProps: GetStaticProps = async () => {
+    //recebimento de post prismic de forma static
+    const prismic = getPrismicClient()
+    
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'post')
+    ], {
+        fetch: ['post.title', 'post.content'],
+        pageSize: 100,
+    })
+
+    //formatação de dados prismic
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-br', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        };
+    });
+
+
+    return{
+        props:{
+            posts
+        }
+    }
+}
