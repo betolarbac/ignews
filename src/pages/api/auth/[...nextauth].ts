2a44f11d72a03d22c48ceaf41620b6bda66822b0
betolarbac <betocr.dev@gmail.com>
2021-12-22 21:22:28 -0300

    erro ao retornar valor active do fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import {query as q } from 'faunadb';
+import {Intersection, query as q } from 'faunadb';
 
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
@@ -21,6 +21,44 @@ export default NextAuth({
   ],
  
   callbacks: {
+    async session({session}) {
+      try{
+        const userActiveSubscription = await fauna.query(
+        q.Get(
+          q.Intersection( [
+            q.Match(
+              q.Index('subscription_by_user_ref'),
+               q.Select(
+                 "ref",
+                 q.Get(
+                   q.Match(
+                     q.Index('user_by_email'),
+                     q.Casefold(session.user.email)
+                   )
+                 )
+               ) 
+            ),
+            q.Match(q.Index('subscription_by_status'), 
+            "active"
+            )
+          ])
+         )
+        )
+      
+
+  
+  
+          return {
+            ...session,
+            activeSubscription: userActiveSubscription
+          } 
+        } catch{
+          return {
+            ...session,
+            activeSubscription: null,
+          }
+        }
+       },
         async signIn({user, account, profile}){
             const { email } = user
 
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,13 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import Prismic from '@prismicio/client'
 
 import { RichText} from 'prismic-dom'
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+
 
 //tipagem de posts
 type Post = {
@@ -31,11 +33,13 @@ export default function Posts({posts}: PostProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map(post => (
-                         <a key={post.slug} href="#">
+                        <Link href={`/posts/${post.slug}`}>
+                         <a key={post.slug}>
                             <time>{post.updatedAt}</time>
                             <strong>{post.title}</strong>
                             <p>{post.excerpt}</p>
                         </a>
+                        </Link>
                     ))}
 
                 </div>
